5db6212b597135773f0ded5a1bc5ed3e606b2238
Talgarbek00 <talgarbeknazarbekov00@gmail.com>
2022-04-02 11:16:36 +0600

    commit 1

---
+++ src/main/java/com/example/gitconflikt/GitConfliktApplication.java
@@ -0,0 +1,18 @@
+package com.example.gitconflikt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitConfliktApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitConfliktApplication.class, args);
+        myMethod();
+    }
+
+    public static void myMethod(){
+
+    }
+
+}
---
+++ src/test/java/com/example/gitconflikt/GitConfliktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitconflikt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitConfliktApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
